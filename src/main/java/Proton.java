b16b81f6782d518d1ffcca285f1afbe49d693d6b
unknown <shrikkanthb@gmail.com>
2026-08-24 23:25:34 +0800

    Rename chatbot to Proton and add greeting

---
+++ src/main/java/Proton.java
@@ -0,0 +1,15 @@
+public class Proton {
+    public static void main(String[] args) {
+        String banner = " ____            _              \n"
+                + "|  _ \\ _ __ ___ | |_ ___  _ __ \n"
+                + "| |_) | '__/ _ \\| __/ _ \\| '_ \\\n"
+                + "|  __/| | | (_) | || (_) | | | |\n"
+                + "|_|   |_|  \\___/ \\__\\___/|_| |_|\n";
+        System.out.println(banner);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hey there! I'm Proton, your positively charged chatbot!");
+        System.out.println("I'm fired up and ready to help! What awesome thing shall we tackle today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Powering down for now, I'll see you next time!");
+    }
+}
