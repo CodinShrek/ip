d12ee0158f1f8e62852b15896656bd4052bc56d5
unknown <shrikkanthb@gmail.com>
2026-08-26 04:35:38 +0800

    Add echo

--- src/main/java/Proton.java
+++ src/main/java/Proton.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Proton {
     public static void main(String[] args) {
         String banner = " ____            _              \n"
@@ -5,11 +7,35 @@ public class Proton {
                 + "| |_) | '__/ _ \\| __/ _ \\| '_ \\\n"
                 + "|  __/| | | (_) | || (_) | | | |\n"
                 + "|_|   |_|  \\___/ \\__\\___/|_| |_|\n";
+
+        String Seperator = "____________________________________________________________";
+
         System.out.println(banner);
-        System.out.println("____________________________________________________________");
+        System.out.println(Seperator);
         System.out.println("Hey there! I'm Proton, your positively charged chatbot!");
         System.out.println("I'm fired up and ready to help! What awesome thing shall we tackle today?");
-        System.out.println("____________________________________________________________");
+        System.out.println(Seperator);
         System.out.println("Powering down for now, I'll see you next time!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String inputCommand = scanner.nextLine();
+
+            System.out.println(Seperator);
+
+            if (inputCommand.equals("bye")) {
+                System.out.println("Powering down for now, I'll see you next time!");
+                System.out.println(Seperator);
+                break;
+            }
+
+            System.out.println(" " + inputCommand);
+            System.out.println(Seperator);
+
+        }
+
+        scanner.close();
+
     }
 }
