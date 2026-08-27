69c1c0b643c02a7b0fe19487417e1b09354008f6
unknown <shrikkanthb@gmail.com>
2026-08-27 17:13:21 +0800

    Add support for list

--- src/main/java/Proton.java
+++ src/main/java/Proton.java
@@ -8,31 +8,39 @@ public class Proton {
                 + "|  __/| | | (_) | || (_) | | | |\n"
                 + "|_|   |_|  \\___/ \\__\\___/|_| |_|\n";
 
-        String Seperator = "____________________________________________________________";
+        String SEPERATOR = "____________________________________________________________";
 
         System.out.println(banner);
-        System.out.println(Seperator);
+        System.out.println(SEPERATOR);
         System.out.println("Hey there! I'm Proton, your positively charged chatbot!");
         System.out.println("I'm fired up and ready to help! What awesome thing shall we tackle today?");
-        System.out.println(Seperator);
-        System.out.println("Powering down for now, I'll see you next time!");
+        System.out.println(SEPERATOR);
 
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         while (scanner.hasNextLine()) {
             String inputCommand = scanner.nextLine();
 
-            System.out.println(Seperator);
+            System.out.println(SEPERATOR);
 
             if (inputCommand.equals("bye")) {
-                System.out.println("Powering down for now, I'll see you next time!");
-                System.out.println(Seperator);
+                System.out.println(" Powering down for now, I'll see you next time!");
+                System.out.println(SEPERATOR);
                 break;
+            } else if (inputCommand.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = inputCommand;
+                taskCount++;
+                System.out.println(" added: " + inputCommand);
             }
 
-            System.out.println(" " + inputCommand);
-            System.out.println(Seperator);
-
+            System.out.println(SEPERATOR);
         }
 
         scanner.close();
